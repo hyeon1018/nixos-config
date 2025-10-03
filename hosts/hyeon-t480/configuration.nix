d0a6d728f8b25cab7fd612023a4668ace0aabf2e
dohyeon kim <hyeon1018@live.com>
2025-10-03 12:29:44 +0900

    Update bluetooth settings

--- hosts/hyeon-t480/configuration.nix
+++ hosts/hyeon-t480/configuration.nix
@@ -16,6 +16,23 @@
     splashImage = "${theme}/splash_image.jpg";
   };
 
+  # bluetooth config
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+    };
+  };
+
   networking.hostName = "hyeon-t480"; # Define your hostname.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
