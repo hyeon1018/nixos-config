5d48becb095eeba044579a279e17cd37ee14b571
dohyeon kim <hyeon1018@live.com>
2025-10-30 18:50:26 +0900

    change gdm to lightdm

--- modules/graphical/gnome/gnome.nix
+++ modules/graphical/gnome/gnome.nix
@@ -3,7 +3,7 @@
 {
   services.xserver.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
 
   environment.gnome.excludePackages = with pkgs; [ ];
 
