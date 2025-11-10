bf63a0afbdc175e18ceafccc14afc2c0971d8036
dohyeon kim <hyeon1018@live.com>
2025-11-10 22:49:11 +0900

    make home manager backupfile is overwrite if already exist

--- home/default.nix
+++ home/default.nix
@@ -3,5 +3,6 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.backupFileExtension = "bak";
+  home-manager.backupFileExtension = "backup";
+  home-manager.overwriteBackup = true;
 }
