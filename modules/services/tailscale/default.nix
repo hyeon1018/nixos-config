b8d3385e0306d17b5e5270017617d7d35d8c7ac6
dohyeon kim <hyeon1018@live.com>
2025-12-12 18:33:57 +0900

    Add trick for tailscale dns problem

--- modules/services/tailscale/default.nix
+++ modules/services/tailscale/default.nix
@@ -7,4 +7,13 @@
 
     ];
   };
+
+  # https://wiki.nixos.org/wiki/Tailscale#DNS
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
+    dnsovertls = "true";
+  };
 }
