35d67d2ec6b0ebd429ff38993ffbff6c671b10a0
dohyeon kim <hyeon1018@live.com>
2026-04-18 00:31:58 +0900

    Fix : IME Candidate window error

--- modules/keyboard/ja.nix
+++ modules/keyboard/ja.nix
@@ -9,6 +9,8 @@
 
   environment.systemPackages = with pkgs; [ ibus ];
 
+  environment.sessionVariables = { MOZC_IBUS_CANDIDATE_WINDOW = "ibus"; };
+
   myConfig.keyboard = {
     inputSources = [ (lib.gvariant.mkTuple [ "ibus" "mozc-jp" ]) ];
   };
