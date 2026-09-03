b5e186b3cf4103e3cb08a5c0ffc3ab0c4a33c5bb
dohyeon kim <hyeon1018@live.com>
2026-09-03 22:56:17 +0900

    System : pipewire enable alsa and jack

--- modules/graphical/gnome/gnome.nix
+++ modules/graphical/gnome/gnome.nix
@@ -10,7 +10,10 @@
   #pipewire
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 
   programs.dconf = {
